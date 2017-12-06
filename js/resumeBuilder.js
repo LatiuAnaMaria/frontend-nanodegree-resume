9560b2dab1d39717a136bb830cf42eb6cd58480e
LatiuAnaMaria <a.latiu@yahoo.com>
2017-12-06 09:29:43 +0200

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,16 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+ */$("main").append("Ana");
+var email = "a.latiu@yahoo.com";
+var newEmail =
+	 email.replace("yahoo","google");
+$("main").append(newEmail);
+console.log (email);
+console.log (newEmail);
+
+var formattedName = HTMLheaderName.replace("%data%", "Ana");
+var role = "web developer";
+var formattedRole = HTMLheaderRole.replace("%data%","student");
+
+$ (#header).append( formattedName);
+$ (#header).prepend( formattedRole);
